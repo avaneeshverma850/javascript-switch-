405de764ae966192b4f6c9513f9df621507d03e1
Avaneesh verma <124239728+avaneeshverma850@users.noreply.github.com>
2023-04-07 21:50:35 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,150 @@
+var day=5
+switch(day){
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday")
+        break
+    case 3:
+        console.log("Wednesday")
+        break
+    case 4:
+        console.log("Thrusday")
+        break
+    case 5:
+        console.log("Friday")
+        break
+    case 6:
+        console.log("Saturday")
+        break
+    case 7:
+        console.log("Sunday")
+        break
+    default:
+        console.log("invalid age")
+        break
+}
+var month=1
+switch(month){
+    case 1:
+        console.log("31 days")
+        break
+    case 2:
+        console.log("28/29 days")
+        break
+    case 3:
+        console.log("31 days")
+        break
+    case 4:
+        console.log("30 days")
+        break
+    case 5:
+        console.log("31 days")
+        break
+    case 6:
+        console.log("31 days")
+        break
+    case 7:
+        console.log("31 days")
+        break
+    case 8:
+        console.log("31 days")
+        break
+    case 9:
+        console.log("30 days")
+        break
+    case 10:
+        console.log("31 days")
+        break
+    case 11:
+        console.log("30 days")
+        break
+    case 12:
+        console.log("31 days")
+        break
+    default:
+        console.log("invalid input")
+    
+} 
+var ch='a'
+switch(ch){
+    case 'a':
+        console.log("vowel")
+        break
+    case 'e':
+        console.log("vowel")
+        break
+    case 'i':
+        console.log("vowel")
+        break
+    case 'o':
+        console.log("vowel")
+        break
+    case 'u':
+        console.log("vowel")
+        break
+    case 'A':
+        console.log("vowel")
+        break
+    case 'E':
+        console.log("vowel")
+        break
+    case 'I':
+        console.log("vowel")
+        break
+    case 'O':
+        console.log("vowel")
+        break
+    case 'U':
+        console.log("vowel")
+        break
+    default:
+        console.log("Consonant")
+}
+var x=34
+switch(x%2){
+    case 0:
+        console.log("even")
+        break
+    case 1:
+        console.log("odd")
+        break
+}
+var x=1
+switch(x){
+    case x>0:
+        console.log("positive")
+        break
+    case x<0:
+        console.log("Negative")
+        break
+    case x==0:
+        console.log("zero")
+        break
+}
+var x=20
+var y=19
+op='&'
+switch(op){
+    case '+':
+        res=x+y
+        console.log(res)
+        break
+    case  '-':
+        res=x-y
+        console.log(res)
+        break
+    case '*':
+        res=x*y
+        console.log(res)
+        break
+    case '/':
+        res=x/y
+        console.log(res)
+        break
+    default:
+        console.log("invalid operator")
+
+}
+
